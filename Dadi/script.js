99e27c7cc9ebd8ef1517f76b9ba1006e7b0a0171
Giovanni <dibellogio@gmail.com>
2024-12-20 14:49:11 +0100

    add logic ex Dadi

--- Dadi/script.js
+++ Dadi/script.js
@@ -1,3 +1,12 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+// ELABORAZIONE
+
+// Creo due variabili una per il giocatore e una per il pc
+// Genero i numeri random e li assegno ai due giocatori
+// Confronto i due numeri 
+    // SE è piu alto il numero del giocatore
+    // Stampo il giocatore ha vinto
+    // ALTRIMENTI
+    // Stampo il pc ha vinto
