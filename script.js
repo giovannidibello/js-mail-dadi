0dce2b6230bc6c9e7d4ba31ab47e4b6f53f9d267
Giovanni <dibellogio@gmail.com>
2024-12-20 13:26:41 +0100

    add logic ex Mail

---
+++ script.js
@@ -0,0 +1,17 @@
+// Mail
+// Crea una lista di email di invitati ad una festa. Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+
+// Creo un array con le email degli invitati
+
+// INPUT
+
+// Chiedo all'utente di inserire la sua email
+
+// ELABORAZIONE
+
+// Creo un ciclo for nel quale confronto il dato inserito con tutti quelli presenti nell'array
+
+// SE la mail fornita è presente nell'array
+    // stampo l'utente è stato invitato
+// ALTRIMENTI
+    // stampo l'utente non è stato invitato
